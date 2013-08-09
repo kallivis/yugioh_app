a359dac5ad1ddbf536f42740773719787322debe
Cody McCarthy <cody.mccarthy@colibri-software.com>
2013-08-09 12:29:18 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YugiohApp::Application.config.secret_key_base = 'a4ddac4d9fa49596203732aaf85f928eef0f4fac1f88f7d3fc31d1906a92d4f10e136594bda25597fb457dcbbdf87ee7344aaebfc8e5adfff5f4529f74d23012'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YugiohApp::Application.config.session_store :cookie_store, key: '_yugioh_app_session'
